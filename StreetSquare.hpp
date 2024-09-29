b9094358fc6109ecbcec9c26857d7d54e9695074
Tsofia Touito <148336183+TsofiaTouito@users.noreply.github.com>
2024-09-29 16:37:05 +0300

    Update StreetSquare.hpp

--- StreetSquare.hpp
+++ StreetSquare.hpp
@@ -1,82 +1,72 @@
-//tsofiatouito2@gmail.com 
+//tsofiatouito2@gmail.com
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <map>
+#include <string>
+#include <cmath>
+#include "Square.hpp"
+#include "Player.hpp"
 
 
-#include "StreetSquare.hpp"
+using namespace std;
 
 
+class StreetSquare : public Square {
 
+    private:
 
-Player* StreetSquare::get_owner(){
-    return this->owner;
-}
+    Player* owner;                      // Pointer to the street owner
+    string streetName;                  // The name of the street (written on the board)
+    int baseRentPrice {75};             // The base rent price before house's building
+    int rentWithHousePrice {85};        // Rent price with one house
+    int rentWithHotelPrice {550};       // Rent price with hotel
+    int streetPrice {250};              // The street's price
+    int housePrice {100};               // House price
+    int hotelPrice {500};               //Hotel price
+    int houses {0};                     // Number of houses
+    bool hasHotel {false};              // True if a hotel exists on the street
 
+    public:
+    string colorGroup;                  // Color group of the street
 
-
-void StreetSquare::set_owner(Player* newOwner){
-    this->owner = newOwner;
-}
-
-
-
-
-int StreetSquare::get_price(){
-    return this->streetPrice;
-}
-
-int StreetSquare::get_house_price(){
-    return this->housePrice;
-}
-
-int StreetSquare::get_hotel_price(){
-    return this->hotelPrice;
-}
-
-void StreetSquare::set_hotel(){
-    this->hasHotel = true;
-}
-
-
-//calaulate the rent price as a result of building houses/ hotels
-int StreetSquare::calc_rent(){
-
-    //If the street is empty from assets, pay the base rent
-    if(houses == 0){
-        return this->baseRentPrice;
-    }
-
-    //If there is no hotel in the street
-    if(!this->hasHotel){
-        double price = (this->rentWithHousePrice)*pow(2, houses -1);
-    }
-
-    //Exist hotel in the street
-    else{
-        return this->rentWithHotelPrice;
-    }
-}
-
-
-//Abilitiy to buy a house in the street aspect
-bool StreetSquare::can_build_house(Player* player){
-        return (this->owner == player) && this->houses < 4 && !this->hasHotel;
-    }
+    // Constructor
+    StreetSquare(const std::string& name, int index, sf::FloatRect square_area, const std::string& street, const std::string& color)
+        : Square(name, index, square_area), streetName(street), colorGroup(color), owner(nullptr) {}
 
 
 
-bool StreetSquare::can_build_hotel(Player* player){
-    return (this->owner == player) && (this->houses == 4) && !this->hasHotel;
-}
+    //Method that checks if the street has an owner
+    Player* get_owner();
+
+    //Set new owner
+    void set_owner(Player*);
+
+    //Return the price of the street
+    int get_price();
+
+    int get_house_price();
+
+    int get_hotel_price();
+
+    void set_hotel();
+
+    //calaulate and return the rent price as a result of building houses/ hotels
+    int calc_rent();
+
+    //Method that checks whether an house can be built
+    bool can_build_house(Player* );
+
+    //Method that checks whether an hotel can be built
+    bool can_build_hotel(Player*);
+
+    //Promote the amount of the houses in the street after building
+    void promote_houses();
+
+    int houses_num();
+
+    int hotel_num();
 
 
-void StreetSquare::promote_houses(){
-    this->houses++;
-}
+};
 
-int StreetSquare::houses_num(){
-    return this->houses;
-}
-
-
-int StreetSquare::hotel_num(){
-    return hasHotel ? 1 : 0;
-}
