d4267135eb4673545ba30cf7bb4c19edfc4f853b
Tsofia Touito <148336183+TsofiaTouito@users.noreply.github.com>
2024-10-01 23:40:23 +0300

    Update StreetSquare.hpp

--- StreetSquare.hpp
+++ StreetSquare.hpp
@@ -2,9 +2,9 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <map>
 #include <string>
 #include <cmath>
+#include <memory>
 #include "Square.hpp"
 #include "Player.hpp"
 
@@ -16,20 +16,20 @@ class StreetSquare : public Square {
 
     private:
 
-    Player* owner;                      // Pointer to the street owner
-    string streetName;                  // The name of the street (written on the board)
+    shared_ptr<Player> owner;           // Pointer to the street owner
     int baseRentPrice {75};             // The base rent price before house's building
     int rentWithHousePrice {85};        // Rent price with one house
     int rentWithHotelPrice {550};       // Rent price with hotel
     int streetPrice {250};              // The street's price
     int housePrice {100};               // House price
     int hotelPrice {500};               //Hotel price
-    int houses {0};                     // Number of houses
     bool hasHotel {false};              // True if a hotel exists on the street
 
     public:
+    string streetName;                  // The name of the street (written on the board)
     string colorGroup;                  // Color group of the street
 
+    int houses {0};                     // Number of houses
     // Constructor
     StreetSquare(const std::string& name, int index, sf::FloatRect square_area, const std::string& street, const std::string& color)
         : Square(name, index, square_area), streetName(street), colorGroup(color), owner(nullptr) {}
@@ -37,10 +37,10 @@ class StreetSquare : public Square {
 
 
     //Method that checks if the street has an owner
-    Player* get_owner();
+    shared_ptr<Player> get_owner();
 
     //Set new owner
-    void set_owner(Player*);
+    void set_owner(shared_ptr<Player>);
 
     //Return the price of the street
     int get_price();
@@ -55,10 +55,10 @@ class StreetSquare : public Square {
     int calc_rent();
 
     //Method that checks whether an house can be built
-    bool can_build_house(Player* );
+    bool can_build_house(shared_ptr<Player> );
 
     //Method that checks whether an hotel can be built
-    bool can_build_hotel(Player*);
+    bool can_build_hotel(shared_ptr<Player>);
 
     //Promote the amount of the houses in the street after building
     void promote_houses();
